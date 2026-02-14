9d8ddfc06e451ee3bde9c8f0c5ffafd15d6431bf
KatiukOleg <katiukoleg@gmail.com>
2026-02-14 19:21:35 +0200

    add reverse_words function

--- tasks/iteration01_strings.py
+++ tasks/iteration01_strings.py
@@ -13,6 +13,24 @@ def reverse_string(string: str) -> str:
     """
     return string[::-1]
 
+def reverse_words(sentence: str) -> str:
+    """
+    Reverse order of words in a sentence.
+    
+    Clarify:
+        - Words are separeted by whitespace.
+
+    Approach:
+        - Split into words, reverse list, join with single spaces.    
+
+    Complexity:
+        - Time: 0(n)
+        - Space: 0(n) for new string
+    """
+    words = sentence.split()
+    return " ".join(reversed(words))    
+
+
 
 if __name__ == "__main__":
-    print(reverse_string("Hello World!"))
+    print(reverse_words("Hello World!"))
--- tests/test_01_strings.py
+++ tests/test_01_strings.py
@@ -1,8 +1,12 @@
 import pytest
 
 from tasks.iteration01_strings import (
-    reverse_string
+    reverse_string,
+    reverse_words
 )
 
 def test_reverse_string_basic():
     assert reverse_string("Hello World!") == "!dlroW olleH"
+
+def test_reverse_words_basic():
+    assert reverse_words("Hello World") == "World Hello"
